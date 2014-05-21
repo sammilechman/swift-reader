4e2c068f900640ef0421db03fa957b7967ca571f
Sam Milechman <sammilechman@gmail.com>
2014-05-21 11:48:46 -0700

    Starting layout, basic CSS applied

---
+++ app/assets/javascripts/speed_reader.js
@@ -0,0 +1,13 @@
+window.SpeedReader = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    // alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  SpeedReader.initialize();
+});
