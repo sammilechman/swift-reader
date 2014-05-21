bb8400ea26d20094a30f9ef9476b08f85819769f
Sam Milechman <sammilechman@gmail.com>
2014-05-21 14:34:03 -0700

    Handle form submit for Reader up

--- app/assets/javascripts/views/welcome_index.js
+++ app/assets/javascripts/views/welcome_index.js
@@ -1,10 +1,46 @@
 SpeedReader.Views.WelcomeIndex = Backbone.View.extend({
   template: JST["welcome/index"],
 
+  initialize: function() {
+    this.speed;
+    this.wordDelay;
+  },
+
+  events: {
+    "submit form": "handleFormSubmit",
+  },
+
   render: function() {
     var renderedContent = this.template();
     this.$el.html(renderedContent);
     return this;
+  },
+
+  calculateWordDelay: function() {
+    console.log(60 / this.speed);
+  },
+
+  handleFormSubmit: function(event) {
+    event.preventDefault();
+    wordsArray = [];
+
+    //The string and speed from the user's input.
+    var words = $(event.currentTarget).serializeJSON().text.body;
+    var speed = $(event.currentTarget).serializeJSON().text.speed;
+
+    //Split by any whitespace to form words array.
+    _(words.split(/\s+/g)).each(function(word) {
+      wordsArray.push(word);
+    });
+
+    this.speed = parseInt(speed);
+    this.calculateWordDelay();
+
+    return this.handleWordsArray(wordsArray);
+  },
+
+  handleWordsArray: function(arr) {
+    _(arr).each(function(word) { console.log(word) });
   },
 
 });
