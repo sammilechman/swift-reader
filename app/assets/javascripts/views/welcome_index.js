b7ed35c9ca4835044388713469627cbefcc52d27
Sam Milechman <sammilechman@gmail.com>
2014-05-21 17:35:24 -0700

    Displaying words nicely - 'centered' on focus letter by using left span/right span

--- app/assets/javascripts/views/welcome_index.js
+++ app/assets/javascripts/views/welcome_index.js
@@ -7,7 +7,7 @@ SpeedReader.Views.WelcomeIndex = Backbone.View.extend({
   },
 
   events: {
-    "submit form": "handleFormSubmit",
+    "submit form": "handleFormSubmit"
   },
 
   render: function() {
@@ -16,31 +16,83 @@ SpeedReader.Views.WelcomeIndex = Backbone.View.extend({
     return this;
   },
 
+  calculateFocusLetter: function(word) {
+    var l = word.length;
+
+    if (l < 2) {
+      return 0;
+    } else if (l < 6) {
+      return 1;
+    } else if (l < 10) {
+      return 2;
+    } else if (l < 14) {
+      return 3;
+    } else {
+      return 4;
+    }
+  },
+
   calculateWordDelay: function() {
-    console.log(60 / this.speed);
+    this.wordDelay = ((60 / this.speed) * 1000);
   },
 
   handleFormSubmit: function(event) {
     event.preventDefault();
-    wordsArray = [];
 
     //The string and speed from the user's input.
     var words = $(event.currentTarget).serializeJSON().text.body;
     var speed = $(event.currentTarget).serializeJSON().text.speed;
 
+    wordsArray = [];
     //Split by any whitespace to form words array.
+    //Might be a better way to do this without if statement.
     _(words.split(/\s+/g)).each(function(word) {
-      wordsArray.push(word);
+      if (word !== "") {
+        wordsArray.push(word);
+      }
     });
 
     this.speed = parseInt(speed);
     this.calculateWordDelay();
 
-    return this.handleWordsArray(wordsArray);
+    return this.renderWordsArray(wordsArray);
   },
 
-  handleWordsArray: function(arr) {
-    _(arr).each(function(word) { console.log(word) });
+  handleFocusLetter: function(word, int) {
+    str = [];
+    str.push("<span class='focus-letter'>");
+    str.push(word.slice(int, int + 1));
+    str.push("</span>");
+    str.push(word.slice(int + 1));
+
+    //Returns left slice of word, right side with styling span applied.
+    return [word.slice(0, int), str.join("")];
+  },
+
+  renderWordsArray: function(wordsArr) {
+    var view = this;
+
+    wordInterval = window.setInterval(function() {
+      if (wordsArr.length == 0) {
+        window.clearInterval(wordInterval);
+      }
+      shiftOff(wordsArr);
+    },this.wordDelay);
+
+    function shiftOff(arr) {
+      if (arr.length == 0) {
+        $("#reader-words").text("");
+        return;
+      } else {
+        var word = arr.shift();
+        console.log(word);
+        var focusLetterPos = view.calculateFocusLetter(word);
+        var words = view.handleFocusLetter(word, focusLetterPos);
+
+        $("#reader-words-left").html(words[0]);
+        $("#reader-words-right").html(words[1]);
+      }
+    };
   },
 
 });
