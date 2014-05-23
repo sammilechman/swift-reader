7aa65f647ed56d7c0e66145e25a3ec90741bfea2
Sam Milechman <sammilechman@gmail.com>
2014-05-22 17:21:57 -0700

    More styling, quotes inserted that render correctly, fix bug where you could doulbe render

--- app/assets/javascripts/views/welcome_index.js
+++ app/assets/javascripts/views/welcome_index.js
@@ -1,19 +1,38 @@
 SpeedReader.Views.WelcomeIndex = Backbone.View.extend({
   template: JST["welcome/index"],
 
+  tagName: "index",
+
   initialize: function() {
     this.speed;
     this.wordDelay;
+    this.inRenderProcess = false;
   },
 
   events: {
-    "submit form": "handleFormSubmit"
+    "keyup form input#input-speed-box": "alignSliderAndInput",
+    // "keyup form input#input-speed-box": "triggerSliderChange",
+    "click form input#speed-select-slider": "alignSliderAndInput",
+    "submit form": "handleFormSubmit",
+    "click .quote": "handleQuoteClick",
   },
 
   render: function() {
     var renderedContent = this.template();
     this.$el.html(renderedContent);
     return this;
+  },
+
+  // triggerSliderChange: function() {
+  //   $("#input-speed-box").trigger("change");
+  // },
+
+  alignSliderAndInput: function(event) {
+    // alert("CHANGE")
+    var speed = $(event.currentTarget).val();
+    console.log(speed);
+    $("#input-speed-box").val(speed);
+    $("#speed-select-slider").val(speed);
   },
 
   calculateFocusLetter: function(word) {
@@ -38,6 +57,9 @@ SpeedReader.Views.WelcomeIndex = Backbone.View.extend({
 
   handleFormSubmit: function(event) {
     event.preventDefault();
+
+    if (this.inRenderProcess) { return; }
+    this.inRenderProcess = true;
 
     //The string and speed from the user's input.
     var words = $(event.currentTarget).serializeJSON().text.body;
@@ -69,12 +91,25 @@ SpeedReader.Views.WelcomeIndex = Backbone.View.extend({
     return [word.slice(0, int), str.join("")];
   },
 
+  handleQuoteClick: function(event) {
+    event.preventDefault();
+    var thisQuote = this.quoteData[event.currentTarget.id];
+    var thisSpeed = $(event.currentTarget).data("wpm")
+
+    $("#text-area-box-input").val(thisQuote);
+    $("#input-speed-box").val(thisSpeed);
+    $("#speed-select-slider").val(thisSpeed);
+
+    $("#text-input-form").submit();
+  },
+
   renderWordsArray: function(wordsArr) {
     var view = this;
 
     var wordInterval = window.setInterval(function() {
       if (wordsArr.length == 0) {
         window.clearInterval(wordInterval);
+        view.inRenderProcess = false;
       }
       shiftOff(wordsArr);
     },this.wordDelay);
@@ -94,6 +129,19 @@ SpeedReader.Views.WelcomeIndex = Backbone.View.extend({
         $("#reader-words-right").html(words[1]);
       }
     };
+  },
+
+  quoteData: {
+    "cj-app-academy": "Back to work!",
+
+    "alec-baldwin-departed": "I’m gonna go have a smoke right now. You want a smoke? You don’t smoke, do ya, right? What are ya, one of those fitness freaks, huh? Go fuck yourself.",
+
+    "the-emperor-jedi": "Everything that has transpired has done so according to my design. Your friends, up there on the sanctuary moon, are walking into a trap, as is your Rebel fleet. It was I who allowed the Alliance to know the location of the shield generator. It is quite safe from your pitiful little band. An entire legion of my best troops awaits them. Oh, I'm afraid the deflector shield will be quite operational when your friends arrive.",
+
+    "ferris-bueller": "The key to faking out the parents is the clammy hands. It's a good non-specific symptom; I'm a big believer in it. A lot of people will tell you that a good phony fever is a dead lock, but, uh... you get a nervous mother, you could wind up in a doctor's office. That's worse than school. You fake a stomach cramp, and when you're bent over, moaning and wailing, you lick your palms. It's a little childish and stupid, but then, so is high school.",
+
+    "denzel-washington": "S.I.S. Detective. Give me 18 months, I'll give you a career. We're an elite unit. We make the big seizures. We make the big arrests. But if you're in my unit, you gotta be in it all the way or not at all. I thought that you was man enough to face that. I guess I was wrong. Five proven, decorated officers say that you're the shooter. The investigators are gonna want to pull a tube of your blood to check for intoxicants, and what are they gonna find, Jake? Do the math. You've been smoking PCP all day, haven't you?",
+
   },
 
 });
